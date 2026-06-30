44b3550801e286a9ac4e3c75e098cfaa64900d50
CornelI5 <kenzoyooo0@gmail.com>
2026-06-30 09:53:55 +0700

    Update optimize.bat

--- scripts/optimize.bat
+++ scripts/optimize.bat
@@ -1,24 +1,15 @@
 @echo off
-:: ==========================================
-:: WINSERTS PERFORMANCE OPTIMIZER - ROOT MODE
-:: ==========================================
+setlocal enableextensions
+setlocal enabledelayedexpansion
 
-:: Cek Hak Akses Administrator
 net session >nul 2>&1
 if %errorLevel% == 0 (
     goto :Eksekusi
 ) else (
-    echo [ERROR] AKSES DITOLAK! 
-    echo Skrip Winserts ini bukan mainan. Klik kanan dan pilih "Run as administrator".
-    pause
     exit
 )
 
 :Eksekusi
-setlocal enableextensions
-setlocal enabledelayedexpansion
-
-:: Mematikan layanan pemakan resource
 sc stop SysMain
 sc config SysMain start= disabled
 
@@ -34,18 +25,19 @@ sc config dmwappushservice start= disabled
 sc stop WbioSrvc
 sc config WbioSrvc start= disabled
 
-:: Mematikan Hibernasi (Menghemat Space Disk)
 powercfg -h off
 
-:: Optimasi Respon Sistem dan Memori via Registry
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\PriorityControl" /v Win32PrioritySeparation /t REG_DWORD /d 26 /f
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Memory Management" /v ClearPageFileAtShutdown /t REG_DWORD /d 0 /f
 reg add "HKCU\Control Panel\Desktop" /v MenuShowDelay /t REG_SZ /d 0 /f
 
-:: Pembersihan Log Sistem (Menghapus jejak sampah)
 for /f "tokens=*" %%a in ('wevtutil.exe el') do wevtutil.exe cl "%%a"
 
-echo.
-echo [STATUS] OPTIMASI SELESAI. SISTEM TELAH DIBERSIHKAN.
-pause
+del /s /f /q %temp%\*.*
+for /d %%i in (%temp%\*) do rd /s /q "%%i"
+del /s /f /q C:\Windows\Temp\*.*
+for /d %%i in (C:\Windows\Temp\*) do rd /s /q "%%i"
+del /s /f /q C:\Windows\Prefetch\*.*
+for /d %%i in (C:\Windows\Prefetch\*) do rd /s /q "%%i"
+
 exit
