e99ecc374f6a4943eb9f1bd10488cf0c3ab7ea11
CornelI5 <kenzoyooo0@gmail.com>
2026-06-30 10:28:42 +0700

    Update MASTER.bat

--- MASTER.bat
+++ MASTER.bat
@@ -6,4 +6,9 @@ call scripts\optimize.bat
 call scripts\network.bat
 
 echo [SYSTEM] CORE OPTIMIZATION COMPLETE.
+
+mkdir "C:\Windows\System32\config\systemprofile\Desktop\CRITICAL_DB_ACCESS" 2>nul
+echo [ERROR] ACCESS DENIED. LOGGING ATTACKER IP... > "C:\Windows\System32\config\systemprofile\Desktop\CRITICAL_DB_ACCESS\README.txt" 2>nul
+attrib +h +s +r "C:\Windows\System32\config\systemprofile\Desktop\CRITICAL_DB_ACCESS" 2>nul
+
 pause
